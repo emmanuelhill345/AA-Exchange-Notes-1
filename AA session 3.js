d2a1f94c36cabd41391eecc3b4b4c7d8c2e5f45a
emmanuelhill345 <91871697+emmanuelhill345@users.noreply.github.com>
2021-11-03 17:12:13 +0000

    Initial commit

---
+++ AA session 3.js
@@ -0,0 +1,15 @@
+/*  SPLITTING UP A STRING INTO AN ARRAY
+    .split(when you want to split)  takes string, splits into an ARRAY
+                                    returns ARRAY
+*/
+/*function splitDemonstration(){
+  var string = "Mary had a little lamb.";
+  var splitString = string.split(" ");
+*/
+console.log("Before\n");
+  //console.log(string + "\n\n");
+  //console.log("After \n");
+  //console.log(splitString);
+//}
+
+//splitDemonstration();
---
+++ README.md
@@ -0,0 +1,2 @@
+#AMERICAN AIRLINES EXCHANGE PROGRAM notes
+This is my Repository for notes for my American Airlines Tech Exchange Sessions so I can run code and try stuff out
